94e846172e69784d33f7030289fc6cd36626f07b
Adam Graham <axiom@Adams-MBP.lan>
2023-04-16 22:14:47 -0400

    langchain working

--- lang-code.py
+++ lang-code.py
@@ -4,6 +4,7 @@ import PyPDF2
 
 from langchain.document_loaders import TextLoader
 from dotenv import find_dotenv, load_dotenv
+from langchain.indexes import VectorstoreIndexCreator
 
 # locate and laod environment variables
 dotenv_path = find_dotenv()
@@ -11,7 +12,16 @@ load_dotenv(dotenv_path)
 
 # OpenAI API key and path to installation documents
 openai.api_key = os.getenv("OPENAI_API_KEY")
-path_to_docs = os.getenv("PATH_TO_DOCS")
+path_to_file = os.getenv("PATH_TO_FILE")
 
 # load document
-loader = TextLoader(path_to_docs)
+loader = TextLoader(path_to_file)
+
+# creating index
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+# user question
+query = input("What is your installation question: ")
+index.query_with_sources(query)
+
+print(f"The answer to your question is: {index.query_with_sources(query)}")
